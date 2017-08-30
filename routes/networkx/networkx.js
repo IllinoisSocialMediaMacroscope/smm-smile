f1be54e92a401642a1c2bf59c4d0e5805c302bd4
Your Name <you@example.com>
2017-08-30 15:45:54 -0500

    network prune isolates pair

--- routes/networkx/networkx.js
+++ routes/networkx/networkx.js
@@ -16,13 +16,13 @@ router.get('/networkx',function(req,res,next){
 });
 
 router.post('/networkx',function(req,res,next){
-	
 	var options = {
 		pythonPath:process.env.PYTHONPATH,
 		scriptPath:appPath + '/scripts/NetworkX/',
 		args:[	'--file',process.env.ROOTDIR + process.env.DOWNLOAD_GRAPHQL + '/'+   req.body.filename, 
 				'--layout',req.body.layout, 
 				'--relationships',req.body.relationships, 
+				'--prune',req.body.prune
 				//'--node_size',req.body.node_size,
 				//'--edge_width',req.body.edge_width 
 			] 
@@ -30,8 +30,8 @@ router.post('/networkx',function(req,res,next){
 	
 	pythonShell.run('network_analysis.py',options,function(err,results){
 		if (err){
-			//throw err;
-			res.send({'ERROR':err});
+			throw err;
+			//res.send({'ERROR':err});
 		}else{
 			
 			var d3js = results[1];
