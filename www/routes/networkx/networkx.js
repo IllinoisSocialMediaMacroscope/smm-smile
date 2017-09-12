9c34d435c29e4379b701b96c6961c53061566454
Your Name <you@example.com>
2017-09-12 15:50:25 -0500

    add a sliencing on python

--- www/routes/networkx/networkx.js
+++ www/routes/networkx/networkx.js
@@ -20,6 +20,7 @@ router.post('/networkx',function(req,res,next){
 	var options = {
 		//pythonPath:'C:/Users/cwang138/AppData/Local/Programs/Python/Python36-32/python.exe',
 		pythonPath:'/opt/python/bin/python3.4',
+		pythonOptions:['-W ignore'],
         scriptPath:appPath + '/scripts/NetworkX/',
 		args:[	'--file','./downloads/GraphQL/'+   req.body.filename, 
 				'--layout',req.body.layout, 
--- www/routes/scikit-learn/scikit-learn-cluster.js
+++ www/routes/scikit-learn/scikit-learn-cluster.js
@@ -20,6 +20,7 @@ router.post('/sklearn-cluster',function(req,res,next){
 	var options = {
 		//pythonPath:'C:/Users/cwang138/AppData/Local/Programs/Python/Python36-32/python.exe',
 		pythonPath:'/opt/python/bin/python3.4',
+		pythonOptions:['-W ignore'],
         scriptPath:appPath + '/scripts/ML/',
 		args:['--file','./downloads/GraphQL/'+   req.body.filename, '--estimator',req.body.model,'--n_clusters',req.body.n_clusters,'--fields']
 	}; 
