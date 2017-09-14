0400816dc20baa181d3ab2ff5b2d3ece73d3ee81
Your Name <you@example.com>
2017-09-14 14:04:23 -0500

    add precaution of no data

--- www/routes/render.js
+++ www/routes/render.js
@@ -11,8 +11,12 @@ router.post('/render',function(req,res,next){
 		
 		var preview_string = fs.readFileSync('./downloads/GraphQL/' + req.body.directory +
 					'/' + req.body.foldername + '/' + req.body.foldername + '.csv', "utf8");
-		var preview_arr = CSV.parse(preview_string);
-		res.send({preview:preview_arr.slice(0,10)}); // preview the top 25 line?
+		if (preview_string === ''){
+			res.send({ERROR: 'This dataset you selected is empty, please select another one!'});
+		}else{
+			var preview_arr = CSV.parse(preview_string);
+			res.send({preview:preview_arr.slice(0,10)}); // preview the top 25 line?
+		}
 			
 		/*else if (req.body.filename.substr(-4) === '.zip'){
 			
