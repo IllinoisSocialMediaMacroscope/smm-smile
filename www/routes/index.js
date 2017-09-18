7c9caccd62e1a33354a8ee332a25f77c5214d97c
Wang <cwang138@illinois.edu>
2017-09-18 16:49:49 -0500

    change homepage layout based on ashley's suggest

--- www/routes/index.js
+++ www/routes/index.js
@@ -7,22 +7,24 @@ router.get('/',function(req,res,next){
 			/*{name:'search',url:'/query',imgURL:'bootstrap/img/logo/search-logo.svg',
 						introduction:`placeholder`
 			},*/
+			{
+			name:'Search Social Media', url:'query', imgURL:'bootstrap/img/logo/search-smm.png',
+			wiki:'https://en.wikipedia.org/wiki/Social_search',
+			introduction: `Our <b>social media search engine</b> enables searching <a href="https://en.wikipedia.org/wiki/User-generated_content">
+							user-generated content</a> such as tweets, user account, comments, and etc from various social meida and network platform. 
+							Both live and cached data is available.`
+			},
 			{name:'Pre-processing',url:'NLP-preprocess', imgURL:'bootstrap/img/logo/NLP/NLP-preprocess.png',
 						wiki:'http://pages.cs.wisc.edu/~jerryzhu/cs769/text_preprocessing.pdf',
 						introduction:`<a href="https://en.wikipedia.org/wiki/Text_segmentation#Word_segmentation"><b>Tokenization</b></a>
 						 is the process of dividing written text into meaningful units, such as words, <a href="https://en.wikipedia.org/wiki/Sentence_(linguistics)">sentences
-						</a>, or <a href="https://en.wikipedia.org/wiki/Topic_(linguistics)" title="Topic (linguistics)">topics</a>.<br><br>
-						
-						<a href="https://nlp.stanford.edu/IR-book/html/htmledition/stemming-and-lemmatization-1.html"><b>Lemmatization and Stemming :</b></a>
-						For grammatical reasons, documents are going to use different forms of a word, such as <I>"organize"</I>, <I>"organizes"</I>, and <I>"organizing"</I>.  
-						Additionally, there are families of derivationally related words with similar meanings, such as <I>"democracy"</I>, <I>"democratic"</I>, and <I>"democratization"</I>.
-						The goal of both stemming and lemmatization is to reduce inflectional forms and sometimes derivationally related forms of a word to a common base form.<br><br>
-						
-						<a href="https://en.wikipedia.org/wiki/Part-of-speech_tagging"><b>Part-of-speech Tagging</b></a> is the process of marking up a word in a text (corpus) as corresponding to a particular 
+						</a>, or <a href="https://en.wikipedia.org/wiki/Topic_(linguistics)" title="Topic (linguistics)">topics</a>.
+						<a href="https://nlp.stanford.edu/IR-book/html/htmledition/stemming-and-lemmatization-1.html"><b>Lemmatization and Stemming </b></a>
+						reduces word forms to common base words.<a href="https://en.wikipedia.org/wiki/Part-of-speech_tagging">
+						<b>Part-of-speech Tagging</b></a> is the process of marking up a word in a text 
+						(corpus) as corresponding to a particular 
 						<a href="https://en.wikipedia.org/wiki/Parts_of_speech">part of speech</a>, based on both 
-						its definition and its context. A simplified form of this is commonly taught to school-age children, in the identification of 
-						words as <a href="https://en.wikipedia.org/wiki/Noun">nouns</a>, <a href="https://en.wikipedia.org/wiki/Verb">verbs</a>, 
-						<a href="https://en.wikipedia.org/wiki/Adjective">adjectives</a>, <a href="https://en.wikipedia.org/wiki/Adverb">adverbs</a>, etc.`},
+						its definition and its context.`},
             {name:'Sentiment Analysis', url:'NLP-sentiment', imgURL:'bootstrap/img/logo/NLP/SA.png',
 						wiki:'https://en.wikipedia.org/wiki/Sentiment_analysis',
 						introduction:`<b>Sentiment analysis</b> (sometimes known as opinion mining or emotion AI) refers to the
@@ -30,15 +32,7 @@ router.get('/',function(req,res,next){
 						natural language processing</a>, <a href="https://en.wikipedia.org/wiki/Sentiment_analysis/wiki/Text_analytics" >text analysis</a>, 
 						<a href="https://en.wikipedia.org/wiki/Sentiment_analysis/wiki/Computational_linguistics">computational linguistics</a>, and 
 						<a href="https://en.wikipedia.org/wiki/Sentiment_analysis/wiki/Biometrics">biometrics</a> to systematically identify, extract, 
-						quantify, and study affective states and subjective information.<br><br>
-
-						<a href="http://comp.social.gatech.edu/papers/icwsm14.vader.hutto.pdf"><b>VADER sentiment analysis</b></a>
-						is a simple <a href="https://en.wikipedia.org/wiki/Rule-based_system">rule-based model</a> for general sentiment analysis. Using a 
-						combination of qualitative and quantitative methods, VADER first construct and empirically validate a 
-						goldstandard list of <a href="https://en.wikipedia.org/wiki/Lexical_semantics">lexical features</a>
-						(along with their associated sentiment intensity measures) which are specifically attuned to sentiment in 
-						<a href="https://en.wikipedia.org/wiki/Microblogging">microblog-like contexts</a>. It then combines these lexical features with consideration for five general rules that embody grammatical and syntactical 
-						conventions for expressing and emphasizing sentiment intensity. `},
+						quantify, and study affective states and subjective information.`},
 			/*{name:'Topic Modeling', url:'/NLP/topic', imgURL:'bootstrap/img/logo/NLP/topic-model.png',
 						wiki:'https://en.wikipedia.org/wiki/Topic_model',
 						introduction:`In <a href="https://en.wikipedia.org/wiki/Machine_learning">machine learning</a> and <a href="https://en.wikipedia.org/wiki/Natural_language_processing">natural language processing</a>, 
@@ -51,27 +45,14 @@ router.get('/',function(req,res,next){
 						The "topics" produced by topic modeling techniques are clusters of similar words.`},*/
 			{name: 'Clustering', url:'sklearn-cluster', imgURL:'bootstrap/img/logo/sklearn/cluster.png',
 						wiki:'https://en.wikipedia.org/wiki/Cluster_analysis',
-						introduction:`<b>Cluster analysis</b> is the task of grouping a set of objects in such a 
-						way that objects in the same group (called a <b>cluster</b>) are more similar (in some sense or another) to each 
-						other than to those in other groups (clusters). It is a main task of exploratory <a href="https://en.wikipedia.org/wiki/Data_mining" title="Data mining">data mining</a>,
-						and a common technique for <a href="https://en.wikipedia.org/wiki/Statistics" title="Statistics">statistical</a> <a href="https://en.wikipedia.org/wiki/Data_analysis" title="Data analysis">
-						data analysis</a>, used in many fields, including <a href="https://en.wikipedia.org/wiki/Machine_learning" title="Machine learning">machine learning</a>,
-						<a href="https://en.wikipedia.org/wiki/Pattern_recognition" title="Pattern recognition">pattern recognition</a>, <a href="https://en.wikipedia.org/wiki/Image_analysis" title="Image analysis">
-						image analysis</a>, <a href="https://en.wikipedia.org/wiki/Information_retrieval" title="Information retrieval">information retrieval</a>, <a href="https://en.wikipedia.org/wiki/Bioinformatics"
-						title="Bioinformatics">bioinformatics</a>, <a href="https://en.wikipedia.org/wiki/Data_compression" title="Data compression">data compression</a>, 
-						and <a href="https://en.wikipedia.org/wiki/Computer_graphics" title="Computer graphics">computer graphics</a>.Cluster analysis itself is not one specific <a href="https://en.wikipedia.org/wiki/Algorithm" title="Algorithm">algorithm</a>, but the general task to be solved. 
-						`},
+						introduction:`<b>Cluster analysis</b> Cluster analysis is the task of grouping a set of objects (for example, words or topics) 
+						in such a way that objects in the same group or cluster are more similar to each other than to those in other groups.`},
 			{name: 'Network Analysis', url:'networkx', imgURL:'bootstrap/img/logo/networkX-logo.png',
 						wiki:"https://en.wikipedia.org/wiki/Social_network_analysis",
 						introduction:`<b>Social network analysis</b> is the process of investigating social 
 						structures through the use of <a href="https://en.wikipedia.org/wiki/Network_theory">networks</a> and <a href="https://en.wikipedia.org/wiki/Graph_theory">graph theory
 						</a>.It characterizes networked structures in terms of <i>nodes</i> (individual actors, people, or things within the network) and the <i>ties</i>, <i>edges</i>, 
-						or <i>links</i> (relationships or interactions) that connect them. <br><br>
-						<a href="https://networkx.github.io/"><b>NetworkX</b></a> is a Python package for the creation, manipulation, and study of the structure, dynamics, and functions of complex networks. Good reviews of 
-						the state-of-the-art in the science of complex networks are presented in <a href="https://arxiv.org/abs/cond-mat/0106096">Albert and Barabási</a>, 
-						<a href="http://epubs.siam.org/doi/abs/10.1137/S003614450342480">Newman</a>, and <a href="https://arxiv.org/abs/cond-mat/0106144">Dorogovtsev and Mendes</a>. 
-						For basic graph algorithms,we recommend the texts of <a href="http://dl.acm.org/citation.cfm?id=523106">Sedgewick</a>, and the survey of <a href="http://www.springer.com/us/book/9783540249795">
-						Brandes and Erlebach</a>.`}]
+						or <i>links</i> (relationships or interactions) that connect them.`}]
                       });
 });
 
