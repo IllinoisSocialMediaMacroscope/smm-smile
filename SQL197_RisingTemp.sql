c0798034e4d144d9ec65df1473141af6da472468
longshuicy <cwang138@illinois.edu>
2017-11-15 14:00:28 -0600

    Create SQL197_RisingTemp.sql

---
+++ SQL197_RisingTemp.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather JOIN weather w ON DATEDIFF(weather.date, w.date) = 1 AND weather.Temperature > w.Temperature
