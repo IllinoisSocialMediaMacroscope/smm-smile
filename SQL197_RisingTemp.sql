bdc33658776e3957c898d8faaf61fac065ed0a42
longshuicy <cwang138@illinois.edu>
2017-11-15 14:00:42 -0600

    Delete SQL197_RisingTemp.sql

--- SQL197_RisingTemp.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-SELECT
-    weather.id AS 'Id'
-FROM
-    weather JOIN weather w ON DATEDIFF(weather.date, w.date) = 1 AND weather.Temperature > w.Temperature
