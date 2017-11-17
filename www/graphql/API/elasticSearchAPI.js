de5d2f99cc79a4b38ee141ff1ed7995dd905ae39
Your Name <you@example.com>
2017-11-17 15:10:39 -0600

    elasticsearch streamtweet bug almost done

--- www/graphql/API/elasticSearchAPI.js
+++ www/graphql/API/elasticSearchAPI.js
@@ -1,24 +1,47 @@
 var Promise = require('bluebird');
-var AWS = require('aws-sdk');
+var appendQuery = require('append-query');
+var fetch = require('node-fetch');
+//var AWS = require('aws-sdk');
 //require('dotenv').config();
 
 function elasticSearchAPI(args){
 	
-	AWS.config.update({ region: 'us-west-2' });
+	//AWS.config.update({ region: 'us-west-2' });
 
 	// create an elasticsearch client for your Amazon ES
-	var client = require('elasticsearch').Client({
-	  hosts: [ 'https://search-es-twitter-stream-yugu6rvjulzswrsj2y764qyy5i.us-west-2.es.amazonaws.com' ],
-	  connectionClass: require('http-aws-es')
-	});
-
+	//var client = require('elasticsearch').Client({
+	  //hosts: [ 'https://search-es-twitter-stream-yugu6rvjulzswrsj2y764qyy5i.us-west-2.es.amazonaws.com' ],
+	  //connectionClass: require('http-aws-es')
+	//});
+	/*param = {};
+	param['q'] = 'text:' + args['q'] +' AND lang:en';
+	param['from'] = (args['pageNum'] -1)* args['perPage'];
+	param['size'] = args['perPage'];
+	
+	// filter by date range
+	if ('startDate' in args && 'endDate' in args){
+		param['q'] += ' AND timestamp_ms:>=' + args['startDate'] + ' AND timestamp_ms:<=' + args['endDate'];
+	}
+	
+	// filter by geolocation
+	if ('distance' in args && 'lon' in args && 'lat' in args){
+		param['q'] += ' AND geo_distance:distance:' + args['distance'] + ' AND geo_distance:coordinates.coordinates:[' + args['lon'] + ',' + args['lat'] + ']';
+	}
+	
+	// filter by followers count of the author
+	if ('followers_count' in args){
+		param['q'] += ' AND user.followers_count:>=' + args['followers_count'];
+	}
+	
+	// filter by author status count
+	if ('statuses_count' in args){
+		param['q'] += ' AND user.statuses_count:>=' + args['statuses_count'];
+	}*/
+	
 	var param = {
-		  index: 'twitter',
-		  type: 'tweet',
 		  from: (args['pageNum'] -1)* args['perPage'],
 		  size: args['perPage'],
-		  body: {
-			query: {
+		  query: {
 				bool: {
 					must:[
 						{ match: { 'text':args['q']} },
@@ -27,12 +50,11 @@ function elasticSearchAPI(args){
 					filter:[]
 				}
 			}
-		  }
-	};
+		};
 	
 	// filter by date range
 	if ('startDate' in args && 'endDate' in args){
-		param['body']['query']['bool']['filter'].push(
+		param['query']['bool']['filter'].push(
 			{range:  {
 				"timestamp_ms":
 					{ 
@@ -46,7 +68,7 @@ function elasticSearchAPI(args){
 	
 	// filter by geolocation
 	if ('distance' in args && 'lon' in args && 'lat' in args){
-		param['body']['query']['bool']['filter'].push(
+		param['query']['bool']['filter'].push(
 			{geo_distance: {
 							"distance": args['distance'],
 							"coordinates.coordinates":[args['lon'],args['lat']]
@@ -56,7 +78,7 @@ function elasticSearchAPI(args){
 	
 	// filter by followers count of the author
 	if ('followers_count' in args){
-		param['body']['query']['bool']['filter'].push(
+		param['query']['bool']['filter'].push(
 			{range:  {
 				"user.followers_count":
 					{ 
@@ -68,7 +90,7 @@ function elasticSearchAPI(args){
 	
 	// filter by author status count
 	if ('statuses_count' in args){
-		param['body']['query']['bool']['filter'].push(
+		param['query']['bool']['filter'].push(
 			{range:  {
 				"user.statuses_count":
 					{ 
@@ -79,16 +101,22 @@ function elasticSearchAPI(args){
 	}
 	
 	return new Promise((resolve,reject) =>{
-		
-		client.search(param).then(function (resp) {
-			//console.log(resp.hits.hits);
-			//console.log(resp.hits.hits);
-			//console.log("GRAPHQL SIDE MESSAGE!!!!!!!!!");
-			resolve(resp.hits.hits);
-		}, function (err) {
-			//console.log(err.message);
+		console.log(param);
+		var endpoint = appendQuery('https://search-es-twitter-stream-yugu6rvjulzswrsj2y764qyy5i.us-west-2.es.amazonaws.com/twitter/tweet/_search/');
+		var headers = {
+			'Accept': 'application/json',
+			'Content-Type':'application/json'
+		}
+		fetch(endpoint,{ method:'POST', headers:headers, body:JSON.stringify(param)}).then((res) =>{
+			return res.json();
+		}).then(function(json){
+			//console.log(JSON.stringify(json));
+			resolve(json.hits.hits);
+		}).catch((err) =>{
+			//console.log(err);
 			reject(err);
-		});
+		});				
+		
 	});
 }
 
