ceb26fa00ab7d6ad4536190282d0cab0bf594ea5
Your Name <you@example.com>
2017-12-06 09:33:08 -0600

    fix bugs

--- www/public/bootstrap/js/customized/view_classification.js
+++ www/public/bootstrap/js/customized/view_classification.js
@@ -57,6 +57,7 @@ $(document).ready(function(){
 						// offer crawling for reddit comments modal
 						if(directory === 'reddit-Post' || directory === 'reddit-Historical-Post' || directory === 'reddit-Search'){
 							$("#dataset").val(directory + "/" + foldername + "/" + foldername + ".csv");
+							$("#length").val(numCat_data.length-1);
 							$("#getComment").show();
 						}else{
 							$("#getComment").hide();
