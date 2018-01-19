39eccf949b131070aeb5b1196e0df5eaa2eccde5
Your Name <you@example.com>
2018-01-19 15:22:20 -0600

    rewrote email; fix bugs

--- www/scripts/deactivated/notification.py
+++ www/scripts/deactivated/notification.py
@@ -13,14 +13,36 @@ def notification(toaddr,case,filename):
     
 
     # map the fpath component to History panel names
+    # local/NLP/sentiment/xxxxxxxxxxxxxxxxxxxxxxxx/ => [local,nlp,sentiment,xxxx,space]
+    # [local, GraphQL, reddit-post, aww, space]
+    # 0         1          2        3
     if filename != '':
         fpath = filename.split('/')
-        if fpath[-3] == 'reddit-Post':
-            fpath[-3] = 'Subreddit Posts Title'
-        elif fpath[-3] == 'reddit-Historical-Post':
-            fpath[-3] = 'Reddit Historical Post'
-        elif fpath[-3] == 'reddit-Search':
-            fpath[-3] = 'Reddit Search Posts Title'
+        
+        if fpath[1] == 'GraphQL':
+            fpath[1] == 'Social Media Data'
+        elif fpath[1] == 'NLP':
+            fpath[1] = 'Nature Language Processing'
+        elif fpath[1] == 'ML':
+            fpath[1] = 'Machine Learning ML'
+        elif fpath[1] == 'NW':
+            fpath[1] = 'Network Visualization and Analysis'
+
+        if fpath[2] == 'reddit-Post':
+            fpath[2] = 'Subreddit Posts Title'
+        elif fpath[2] == 'reddit-Historical-Post':
+            fpath[2] = 'Reddit Historical Post'
+        elif fpath[2] == 'reddit-Search':
+            fpath[2] = 'Reddit Search Posts Title'
+        elif fpath[2] == 'sentiment':
+            fpath[2] = 'Sentiment Analysis'
+        elif fpath[2] == 'preprocessing':
+            fpath[2] = 'NLP Preprocessing'
+        elif fpath[2] == 'networkx':
+            fpath[2] = 'Python NetworkX'
+        elif fpath[2] == 'classification':
+            fpath[2] = 'Text Classification'
+            
 
     if case == 0:
         html = """
@@ -28,10 +50,14 @@ def notification(toaddr,case,filename):
             <head></head>
             <body>
 		<div>
-		    <h3>Your Reddit Comment collection has been terminated :-(</h3>
+                    <h3>Dear user (session """ + fpath[0] + """),</h3>
+		    <p>Your Reddit Comment collection has been terminated.</p>
 		    <p>We are using the <b>id</b> and <b>permalink</b> from your Reddit Submission dataset
 		    to collect comments and replies. It is most likely you have provide an incomplete Reddit Submission dataset missing these two fields.</p>
 		    <p>Please try to reproduce the Reddit Submission with <b>id</b> and <b>permalink</b>, or switch to another dataset.</p>
+		    <br>
+                    <p>Best Regards,</p>
+                    <p>Social Media Macroscope - SMILE </p>
 		</div>
             </body>
         </html>
@@ -42,12 +68,16 @@ def notification(toaddr,case,filename):
                     <head></head>
                     <body>
                             <div>
-                                <h3>Your Reddit Comment collection is exceeding 400 Megabyte, and is terminated due to lack of disk space. :-p</h3>
-                                <p>You have requested comments and replies for the Reddit Submission (Post):<u>""" + fpath[-1] + """</u>. The partial comments we manage to
-                                collect and save will be compressed for you in an .zip file named <u>"""+ fpath[-1][:-4]+"""-comments.zip</u></p>    
+                                <h3>Dear user (session """ + fpath[0] + """),</h3>
+                                <p>Your Reddit Comment collection is exceeding 400 Megabyte, and is terminated due to lack of disk space.</p>
+                                <p>You have requested comments and replies for the Reddit Submission (Post):<u>""" + fpath[3] + """</u>. The partial comments we manage to
+                                collect and save will be compressed for you in an .zip file named <u>""" + fpath[3] + """-comments.zip</u></p>    
                                 <p>In order to download this file, you need to first locate the original submission in the <b>HISTORY</b> page in SMILE. Go to <b>History</b> -->
-                                under <b>Social Media Data</b> --> click <b>""" + fpath[-3] + """</b> --> then find <b>""" + fpath[-2] + """</b> --> click <b>VIEW</b> -->
+                                under <b>""" + fpath[1] + """</b> --> click <b>""" + fpath[2] + """</b> --> then find <b>""" + fpath[3] + """</b> --> click <b>VIEW</b> -->
                                 in the <b>Overview</b> table under the <b>downloadables</b> column, you will find these comments in a zip file.</p>
+                                <br>
+                                <p>Best Regards,</p>
+                                <p>Social Media Macroscope - SMILE </p>
                             </div>
                     </body>
             </html>"""
@@ -57,16 +87,46 @@ def notification(toaddr,case,filename):
                     <head></head>
                     <body>
                             <div>
-                                <h3>Your Reddit Comment collection is ready for you! :-D</h3>
-                                <p>You have requested comments and replies for the Reddit Submission (Post):<u>""" + fpath[-1] + """</u>. It will be compressed for you in an
-                                .zip file named <u>"""+ fpath[-1][:-4] +"""-comments.zip</u></p>    
+                                <h3>Dear user (session """ + fpath[0] + """),</h3>
+                                <p>Your Reddit Comment collection is ready for you!</p>
+                                <p>You have requested comments and replies for the Reddit Submission (Post):<u>""" + fpath[3] + """</u>. It will be compressed for you in an
+                                .zip file named <u>"""+ fpath[3] +"""-comments.zip</u></p>    
                                 <p>In order to download this file, you need to first locate the original submission in the <b>HISTORY</b> page in SMILE. Go to <b>History</b> -->
-                                under <b>Social Media Data</b> --> click <b>""" + fpath[-3] + """</b> --> then find <b>""" + fpath[-2] + """</b> --> click <b>VIEW</b> -->
+                                under <b>""" + fpath[1] +"""</b> --> click <b>""" + fpath[2] + """</b> --> then find <b>""" + fpath[3] + """</b> --> click <b>VIEW</b> -->
                                 in the <b>Overview</b> table under the <b>downloadables</b> column, you will find these comments in a zip file.</p>
+                                <br>
+                                <p>Best Regards,</p>
+                                <p>Social Media Macroscope - SMILE </p>
                             </div>
                     </body>
             </html>"""
         subject = 'Your Reddit Comment collection is completed!'
+    elif case == 3:
+
+        for key in output.keys():
+            list_html += '<li><a href="' + output[key] + '">' + key + '</a></li>'
+            
+        html = """<html> 
+                    <head></head>
+                    <body>
+                            <div>
+                                <p>Dear user (session ID: """ + fpath[0] + """),</p>
+                                <p>Your """ + fpath[2] + """ results are ready for you!</p>
+                                <ul>
+                                    <li>You can view the visualization and download the results at <b>HISTORY</b> page in SMILE. Go to <b>History</b> -->
+                                    under <b>""" + fpath[1] + """</b> tab --> click <b>""" + fpath[2] + """</b> --> then find <b>""" + fpath[3] + """</b> --> click <b>view</b></li>
+                                    <li>You can also click the link below to download part of the results:
+                                        <ul>""" + list_html + """</ul>
+                                    </li>
+                                </ul>
+                                <br>
+                                <p>Best Regards,</p>
+                                <p>Social Media Macroscope - SMILE </p>
+                            </div>
+                    </body>
+            </html>"""
+        subject = 'Your ' + fpath[2] + ' is completed!'
+        
         
     password = '***REMOVED***'
 
@@ -80,3 +140,7 @@ def notification(toaddr,case,filename):
     server.login(fromaddr,password)
     server.sendmail(fromaddr, toaddr, msg.as_string())
     server.quit()
+
+if __name__ == '__main__':
+    output = {}
+    notification('***REMOVED***',case=3,filename='local/NLP/sentiment/b10984e0-a716-4913-a9dc-af970d07507e/',output)
