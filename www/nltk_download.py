5f588ea644f69c041a9a5bc536c71dd7ffb255da
Chen Wang <cwang138@illinois.edu>
2023-06-29 15:35:48 -0500

    remove everything related to graphql

--- www/nltk_download.py
+++
@@ -1,6 +0,0 @@
-import nltk
-nltk.download('vader_lexicon',download_dir='../../nltk_data/')
-nltk.download('punkt',download_dir='../../nltk_data/')
-nltk.download('stopwords',download_dir='../../nltk_data/')
-nltk.download('wordnet',download_dir='../../nltk_data/')
-nltk.download('averaged_perceptron_tagger',download_dir='../../nltk_data/')
